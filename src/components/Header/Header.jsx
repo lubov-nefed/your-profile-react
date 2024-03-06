6ad857216453edc1d82d5f4e39d889cb62e59db7
lubov-nefed <love.nefed@gmail.com>
2024-03-06 17:19:10 +0300

    feat: add Header component without styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import logo272w from "./images/logo-icon-272w.svg";
+import burgerIcon from "./images/burger-icon.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container container">
+        <a className="header__link" href="/your-profile-react/">
+          <img
+            className="header__logo header__logo--light-theme"
+            src={logo272w}
+            alt="Your Profile Logo"
+          />
+        </a>
+        <div className="header__btns">
+          <button className="header__theme-btn btn">
+            <img
+              className="theme-btn__img"
+              src="https://raw.githubusercontent.com/lubov-nefed/your-profile/main/images/light-dark-icon.svg"
+              alt="Switch theme"
+            />
+          </button>
+          <a
+            className="header__btn btn header__btn--basic-color-style login-btn"
+            href="${loginPaths[currentPath]}"
+          >
+            Login
+          </a>
+        </div>
+        <img className="header__burger" src={burgerIcon} alt="Open menu" />
+      </div>
+    </header>
+  );
+}
+
+export { Header };
