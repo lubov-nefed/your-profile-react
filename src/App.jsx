d232f9473fe5a14b5da50bad544b55f406024d73
lubov-nefed <love.nefed@gmail.com>
2024-03-08 14:00:25 +0300

    feat: add react-router with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
-import { Header } from "./components/Header/Header.jsx";
-import { Footer } from "./components/Footer/Footer.jsx";
+import { Route, Routes } from "react-router-dom";
+import { CreateProfilePage } from "./pages/create-profile/CreateProfilePage.jsx";
+import { ProfilePreviewPage } from "./pages/profile-preview/ProfilePreviewPage.jsx";
+import { LoginPage } from "./pages/login/LoginPage.jsx";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <Routes>
+      <Route exact path="/" element={HomePage} />
+      <Route path="/login" element={LoginPage} />
+      <Route path="/create-profile" element={CreateProfilePage} />
+      <Route path="/profile-preview" element={ProfilePreviewPage} />
+    </Routes>
   );
 }
 
