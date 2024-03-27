d9d19e9635cb0abd66c53f9cfb91f3a6142262e2
lubov-nefed <love.nefed@gmail.com>
2024-03-27 13:00:29 +0300

    feat: add BurgerMenu

---
+++ src/components/Header-components/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,71 @@
+import { ThemeContext } from "../../../ThemeContext.jsx";
+import { useContext } from "react";
+import { Container } from "../../Container/Container.jsx";
+import { LoginBtn } from "../LoginBtn/LoginBtn.jsx";
+import { ThemeBtn } from "../ThemeBtn/ThemeBtn.jsx";
+import "./BurgerMenu.css";
+
+function BurgerMenu({ SwitchTheme }) {
+  const theme = useContext(ThemeContext);
+  function closeBurgerMenu() {
+    const burgerMenu = document.querySelector(".burger");
+    burgerMenu.classList.remove("opened");
+    burgerMenu.classList.add("closed");
+  }
+  return (
+    <nav className="burger closed" data-theme={theme}>
+      <header className="burger__header">
+        <Container className="header__container">
+          <div className="burger__btns-container">
+            <LoginBtn />
+            <ThemeBtn onClick={SwitchTheme} />
+          </div>
+          <button
+            className="btn burger__close-btn"
+            data-theme={theme}
+            onClick={closeBurgerMenu}
+          >
+            <svg
+              className="burger__close-btn-svg"
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+            >
+              <rect
+                width="35"
+                height="5"
+                x="3.536"
+                rx="2.5"
+                transform="rotate(45 3.536 0)"
+              />
+              <rect
+                width="35"
+                height="5"
+                y="24.749"
+                rx="2.5"
+                transform="rotate(-45 0 24.749)"
+              />
+            </svg>
+          </button>
+        </Container>
+      </header>
+      <ul className="burger__links-list">
+        <li>
+          <a className="burger__link" href="/your-profile-react/create-profile">
+            Create Profile
+          </a>
+        </li>
+        <li>
+          <a
+            className="burger__link"
+            href="/your-profile-react/profile-preview"
+          >
+            Watch examples
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export { BurgerMenu };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import burgerIcon from "./images/burger-icon.svg";
 import "./Header.css";
 import { Container } from "../Container/Container.jsx";
 import { ThemeBtn } from "../Header-components/ThemeBtn/ThemeBtn.jsx";
@@ -6,34 +5,48 @@ import { Logo } from "../Logo/Logo.jsx";
 import { ThemeContext } from "../../ThemeContext.jsx";
 import { useContext } from "react";
 import { LoginBtn } from "../Header-components/LoginBtn/LoginBtn.jsx";
+import { BurgerMenu } from "../Header-components/BurgerMenu/BurgerMenu.jsx";
 
 function Header({ SwitchTheme }) {
   const theme = useContext(ThemeContext);
+  function openBurgerMenu() {
+    const burgerMenu = document.querySelector(".burger");
+    burgerMenu.classList.remove("closed");
+    burgerMenu.classList.add("opened");
+  }
+
   return (
-    <header className="header" data-theme={theme}>
-      <Container className="header__container">
-        <a className="header__link" href="/your-profile-react/">
-          <Logo className="header__logo" />
-        </a>
-        <div className="header__btns">
-          <ThemeBtn onClick={SwitchTheme} />
-          <LoginBtn />
-        </div>
-        <button className="btn header__burger-btn" title="Open menu">
-          <svg
-            className="burger-btn__svg"
-            data-theme={theme}
-            width="20"
-            height="15"
-            xmlns="http://www.w3.org/2000/svg"
+    <>
+      <BurgerMenu SwitchTheme={SwitchTheme} />
+      <header className="header" data-theme={theme}>
+        <Container className="header__container">
+          <a className="header__link" href="/your-profile-react/">
+            <Logo className="header__logo" />
+          </a>
+          <div className="header__btns">
+            <ThemeBtn onClick={SwitchTheme} />
+            <LoginBtn />
+          </div>
+          <button
+            className="btn header__burger-btn"
+            title="Open menu"
+            onClick={openBurgerMenu}
           >
-            <rect y="12" width="20" height="3" rx="1.5" />
-            <rect y="6" width="20" height="3" rx="1.5" />
-            <rect width="20" height="3" rx="1.5" />
-          </svg>
-        </button>
-      </Container>
-    </header>
+            <svg
+              className="burger-btn__svg"
+              data-theme={theme}
+              width="20"
+              height="15"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect y="12" width="20" height="3" rx="1.5" />
+              <rect y="6" width="20" height="3" rx="1.5" />
+              <rect width="20" height="3" rx="1.5" />
+            </svg>
+          </button>
+        </Container>
+      </header>
+    </>
   );
 }
 
